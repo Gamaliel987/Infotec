ee427e01d638bb1636b03fb1785e3dbc641ca054
Gamaliel987 <gadelgado@inea.gob.mx>
2026-01-08 15:22:10 +0000

    Descripción del cambio

--- src/database/migrations/2025_12_11_161029_create_eventos_table.php
+++ src/database/migrations/2025_12_11_161029_create_eventos_table.php
@@ -2,13 +2,10 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+
+
+return new class extends Migration {
+    public function up() {
         Schema::create('eventos', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
@@ -19,11 +16,7 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+    public function down() {
         Schema::dropIfExists('eventos');
     }
 };
